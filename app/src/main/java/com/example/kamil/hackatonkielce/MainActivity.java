879b499e787a5a7398b3aca29b013a523a8b8413
Mateusz <pieczabamateusz8@gmail.com>
2018-06-15 11:52:05 +0200

    3

--- app/src/main/java/com/example/kamil/hackatonkielce/MainActivity.java
+++ app/src/main/java/com/example/kamil/hackatonkielce/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    public static int tak;
+    public static int saj;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
